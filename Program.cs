0227d16e49a345f1a60065139af366a09de3b5ec
qazx4740 <guno7415@gmail.com>
2023-03-31 13:06:26 +0900

    .....

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@
                 Console.WriteLine("헤더입니다");  // A#2
                 Console.WriteLine("회원가입");  // B#4
                 Console.WriteLine("관리자");  // B#6
-                Console.WriteLine("실수...");  // B#6
                 Console.WriteLine("about");  //A#8
+                Console.WriteLine("실수...");  // B#6
+                Console.WriteLine("실수....");  // B#6
             }
             Console.WriteLine("article");
             {
