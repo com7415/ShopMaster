4c658b9d5123444aa1f916bc077fb30fa982e586
com7415 <qazx4740@naver.com>
2023-03-31 10:25:17 +0900

    initial commint

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ShopMaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("header");
+            {
+
+            }
+            Console.WriteLine("article");
+            {
+
+            }
+            Console.WriteLine("footer");
+            {
+
+            }
+        }
+    }
+}
